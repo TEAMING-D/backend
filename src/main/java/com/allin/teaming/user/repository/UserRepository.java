5f0cb8bb0c59a6f57620429247c908e5d569b5a7
osjkate <osjkate@sookmyung.ac.kr>
2024-07-29 02:19:05 +0900

    feat : school 수정

--- src/main/java/com/allin/teaming/user/repository/UserRepository.java
+++ src/main/java/com/allin/teaming/user/repository/UserRepository.java
@@ -3,11 +3,16 @@ package com.allin.teaming.user.repository;
 import com.allin.teaming.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
+    List<User> findByUsername(String username);
+
+    Optional<User> findBySchoolNum(String schoolNum);
     boolean existsByPhone(String Phone);
     boolean existsByEmail(String email);
+
+    boolean existsByUsernameAndSchoolNum(String username, String schoolNum);
 }
--- src/main/java/com/allin/teaming/user/service/UserService.java
+++ src/main/java/com/allin/teaming/user/service/UserService.java
@@ -46,12 +46,19 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
-    // 팀원 검색 (username 으로 회원 조회)
+    // 팀원 검색 (username 으로 회원 전체 조회)
     @Transactional(readOnly = true)
-    public UserDetailDto getUserByUsername(String username) {
-        return userRepository.findByUsername(username)
+    public List<UserSimpleDto> getUserByUsername(String username) {
+        return userRepository.findByUsername(username).stream()
+                .map(UserSimpleDto::of).toList();
+    }
+
+    // 학번으로 조회
+    @Transactional(readOnly = true)
+    public UserDetailDto getUserBySchoolNum(String schoolNum) {
+        return userRepository.findBySchoolNum(schoolNum)
                 .map(UserDetailDto::of)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("해당 학번을 가진 학생을 조회할 수 없습니다. "));
     }
 
 
@@ -62,9 +69,14 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다."));
 
         // 학교 가져오기 -> 리스트에서 선택
-        School school = schoolRepository.findById(request.getSchoolId()).get();
+        School school = null;
+        if (request.getSchoolName() != null && !request.getSchoolName().equals(user.getSchool().getName())) {
+            school = schoolRepository.findByName(request.getSchoolName())
+                    .orElseThrow(() -> new IllegalArgumentException("해당 학교가 존재하지 않습니다."));
+            user.updateSchool(school);
+        }
+        user.update(request.getUsername(), request.getInfo(), request.getMajor(), request.getSchoolNum());
 
-        user.update(request.getUsername(), request.getInfo(), school, request.getMajor());
         return IdResponse.of(user);
     }
 
@@ -72,31 +84,36 @@ public class UserService {
     @Transactional
     public IdResponse signUp(UserRegistDto request) {
         if (userRepository.existsByPhone(request.getPhone())) {
-            throw new RuntimeException("이미 가입되어 있는 전화번호입니다. ");
+            throw new IllegalArgumentException("이미 가입되어 있는 전화번호입니다. ");
         }
 
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new RuntimeException("이미 가입되어 있는 이메일입니다. ");
+            throw new IllegalArgumentException("이미 가입되어 있는 이메일입니다. ");
+        }
+
+        if (userRepository.existsByUsernameAndSchoolNum(request.getUsername(), request.getSchoolNum())) {
+            throw new IllegalArgumentException("이미 가입되어 있는 사용자(학번) 입니다. ");
         }
 
         School school = null;
-        if (request.getSchoolId() != null) {
-            school = schoolRepository.findById(request.getSchoolId()).get();
+        if (request.getSchoolName() != null) {
+            school = schoolRepository.findByName(request.getSchoolName())
+                    .orElseThrow(() -> new IllegalArgumentException("해당 학교를 조회할 수 없습니다. "));
         }
+
         User user = request.toUser(school, bCryptPasswordEncoder.encode(request.getPassword()));
         userRepository.save(user);
         return IdResponse.of(user);
     }
 
     // 탈퇴
-    // TODO: 회원 삭제 시 처리
+    // TODO: 회원 삭제 시 처리 추가
     @Transactional
     public void deleteUser(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습ㄴ디ㅏ. "));
 
         // 업무 자동 삭제
-
         userRepository.delete(user);
     }
 
